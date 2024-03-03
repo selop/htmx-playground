bf98c369cc8b9cf8bfcaec33c6667eaef23abe39
Sergej Lopatkin <lopatkin.sergej@googlemail.com>
2024-03-03 11:54:18 +0100

    initial commit

---
+++ src/main/java/net/lopatkin/htmxplayground/HtmxPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package net.lopatkin.htmxplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmxPlaygroundApplication.class, args);
+	}
+
+}
